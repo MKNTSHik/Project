6aa43a442f0d471d950eba6fa8f48477084698b2
Kovtun Ivan <ivan.opera62@gmail.com>
2025-12-25 11:54:46 +0800

    frontend leads

--- frontend/script.js
+++ frontend/script.js
@@ -15,7 +15,7 @@ form.addEventListener("submit", async function(event) {
         }
         const response = await fetch(url_post_subscribers, {
             method: "POST",
-            headers: {"Content-type": "application/json"},
+            headers: [{"Content-type": "application/json"}],
             body: JSON.stringify(formData)
         });
             if (response.ok) {
@@ -28,14 +28,14 @@ form.addEventListener("submit", async function(event) {
             } finally {
                 submitButton.disabled = false;
             }
-}})
+        }
+    })
 
 
 const tv_shows_Button = document.getElementById("tv_shows_Button");
 const digital_Button = document.getElementById("digital_Button");
 const url_get_tv_shows = "http://localhost:8000/categories/tv_shows"
 const url_get_digital = "http://localhost:8000/categories/digital"
-
 
 
 tv_shows_Button.addEventListener("click", async function() {
@@ -66,3 +66,43 @@ digital_Button.addEventListener("click", async function() {
         console.error(`ошибка при отправке ${error}`);
     } finally {}
 });
+
+
+const leads_Button = document.getElementById("connect_Button");
+const leads_form = document.getElementById("contact_fame");
+const leads_name = document.getElementById("name");
+const leads_email = document.getElementById("email1");
+const leads_message = document.getElementById("message");
+const url_post_leads = "http://localhost:8000/leads";
+
+
+leads_form.addEventListener("submit", async function(event) {
+    event.preventDefault();
+    if(event.submitter === leads_Button) {
+        leads_Button.disabled = true;
+        const name = leads_name.value.trim();
+        const email = leads_email.value.trim();
+        const message = leads_email.value.trim();
+        try {
+            const formData =  {
+                "name": name,
+                "email": email,
+                "message": message
+            }
+            const response = await fetch(url_post_leads, {
+                method: "POST",
+                headers: [{"Content-type": "application/json"}],
+                body: JSON.stringify(formData)
+            });
+            if (response.ok) {
+                console.log("все ок")
+            } else {
+                console.error(await response.text());
+            }
+    } catch(error) {
+                console.error(`ошибка при отправке ${error}`);
+            } finally {
+                submitButton.disabled = false;
+            }
+        }
+    });
